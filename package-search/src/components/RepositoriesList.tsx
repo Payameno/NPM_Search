680256d6982aa4c11dad88b96e2d321991d48146
Payam Beigi <beigi.payam@gmail.com>
2023-02-09 19:17:21 -0800

    Fetch data from api

--- package-search/src/components/RepositoriesList.tsx
+++ package-search/src/components/RepositoriesList.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
 
   const [term, setTerm] = useState('');
-  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(event.target.value);
+  }
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    dispatch(actionCreators.searchRepositories(term));
+    searchRepositories(term);
   }
   return (    
   <div>
     <form onSubmit={onSubmit}>
-      <input />
+      <input onChange={onChange}/>
       <button>Search</button>
     </form>
   </div>
