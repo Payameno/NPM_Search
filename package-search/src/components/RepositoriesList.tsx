3b418cf74cbab1e683075eebe02ba66c3dada3d1
Payam Beigi <beigi.payam@gmail.com>
2023-02-09 18:33:22 -0800

    Dispatch actin using term, searching repository

--- package-search/src/components/RepositoriesList.tsx
+++ package-search/src/components/RepositoriesList.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
+
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    dispatch(actionCreators.searchRepositories(term));
+  }
   return (    
   <div>
-    <form>
+    <form onSubmit={onSubmit}>
       <input />
       <button>Search</button>
     </form>
